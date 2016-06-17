8f6bee0a327e411ade7117e0294fa1e97d676288
Pierig Le Saux <lesaux@pythian.com>
2016-06-17 12:52:26 -0400

    rancher_hosts module cleanup

--- modules/rancher_hosts/jsonval.sh
+++
@@ -1,4 +0,0 @@
-command=`curl http://rancher.linuxdataflow.org/v1/projects/1a5/registrationtokens | jq -r '.data[].command'`
-echo $command
-command=`echo $command|sed s/--privileged/--privileged\ -e\ CATTLE_AGENT-IP=/g`
-echo $command
