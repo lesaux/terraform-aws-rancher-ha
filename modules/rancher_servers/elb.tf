06b201198e2ee6b234b2eb88b9549e0ad30c3ace
Pierig Le Saux <lesaux@pythian.com>
2016-07-05 11:26:10 -0400

    Update elb.tf

--- modules/rancher_servers/elb.tf
+++ modules/rancher_servers/elb.tf
@@ -58,7 +58,7 @@ resource "aws_proxy_protocol_policy" "rancher-external" {
 }
 
 resource "aws_route53_record" "rancher" {
-  zone_id = "Z1TLA2WNUBIE2P"
+  zone_id = "${var.route53_zone_id}"
   name    = "${var.rancher_url}"
   type    = "CNAME"
   ttl     = "60"
